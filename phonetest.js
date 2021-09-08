3c6474b8f5be53a4e8d64a92291b08b9e9f86167
TTVBOUNCYLION <86756080+dylancorsoo@users.noreply.github.com>
2021-09-07 20:51:55 -0400

    Add files via upload

---
+++ phonetest.js
@@ -0,0 +1,7 @@
+const iphone = {screensize: "6.1", data: "5g", camera: "12mp"};
+
+const person = {firstName:"John", lastName:"Doe", age:46};
+
+document.getElementById("demo").innerhTML= iphone.screensize;
+
+document.write("<p>iphone.data</p>");
